82872af84220cca8a2b2af609797e5ae58b7c988
Alessandro Martini <18130319+martini97@users.noreply.github.com>
2024-10-22 15:22:02 -0300

    chore: expose index builder

--- src/globals.ts
+++ src/globals.ts
@@ -1,8 +1,13 @@
 import { MetadataStorage } from "src/metadata/storage";
+import { IndexBuilder } from "src/builder/index-builder";
 
 export function getMetadataStorage(): MetadataStorage {
   if (!global.metadataStorage) {
     global.metadataStorage = new MetadataStorage();
   }
   return global.metadataStorage;
+}
+
+export function getIndexBuilder(): IndexBuilder {
+  return new IndexBuilder(getMetadataStorage());
 }
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import "reflect-metadata";
 
 export { SearchEntity } from "./decorators/search-entity";
 export { SearchField } from "./decorators/search-field";
+export { type IndexBuilder } from "./builder/index-builder";
+export { getIndexBuilder } from "./globals";
